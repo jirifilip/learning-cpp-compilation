4f18db06fc0dd4796f82d1f71a7569b71374fe5c
Jiří Filip <j.f.ilip@seznam.cz>
2024-12-05 14:36:06 +0100

    Add basic project structure

---
+++ test_cases/including_cpp_files/test.Tests.ps1
@@ -0,0 +1,17 @@
+﻿$script:here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+Describe "deploy" {
+    It "Returns expected output" {
+        $outPath = "$here/out"
+
+        if (-Not (Test-Path $outPath)) {
+            mkdir $outPath
+        }
+
+        g++ "$here/main.cpp" -o "$outPath/main.exe"
+
+        $output = . "$outPath/main.exe"
+        
+        $output | Should -Be "Hello world"
+    }
+}
