d8a294a8ac18231f859fcc82872209a9825db9db
Jiří Filip <j.f.ilip@seznam.cz>
2024-12-05 14:39:01 +0100

    Rewrite basic test

--- test_cases/including_cpp_files/test.Tests.ps1
+++ test_cases/including_cpp_files/test.Tests.ps1
@@ -1,13 +1,15 @@
 ﻿$script:here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$script:outPath = "$here/out"
+
+BeforeAll {
+    if (-Not (Test-Path $outPath)) {
+        mkdir $outPath
+    }
+}
+
 
 Describe "deploy" {
     It "Returns expected output" {
-        $outPath = "$here/out"
-
-        if (-Not (Test-Path $outPath)) {
-            mkdir $outPath
-        }
-
         g++ "$here/main.cpp" -o "$outPath/main.exe"
 
         $output = . "$outPath/main.exe"
