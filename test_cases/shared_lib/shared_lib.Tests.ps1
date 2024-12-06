a27acd38af89f189f43a71ba4a8d03c3589eaea8
Jiří Filip <j.f.ilip@seznam.cz>
2024-12-06 15:09:07 +0100

    More experiments with library loading

--- test_cases/shared_lib/shared_lib.Tests.ps1
+++ test_cases/shared_lib/shared_lib.Tests.ps1
@@ -8,14 +8,27 @@
         }
 
         mkdir $outPath
+
+        g++ -shared "$here/src/lib.cpp" -o "$outPath/lib.dll"
+        g++ -shared "$here/src/lib_alternative.cpp" -o "$outPath/lib_alternative.dll"
+        g++ -w "$here/src/main.cpp" -o "$outPath/main.exe"
     }
 
     It "loads library dynamically" {
-        g++ -shared "$here/src/lib.cpp" -o "$outPath/lib.dll"
-
-        g++ -w "$here/src/main.cpp" -o "$outPath/main.exe"
         $output = . "$outPath/main.exe"
         
         $output | Should -Be "1 + 2 = 3"
+    }
+
+    It "loads library 'lib.dll' when provided as an argument" {
+        $output = . "$outPath/main.exe" "lib.dll"
+        
+        $output | Should -Be "1 + 2 = 3"
+    }
+
+    It "loads library 'lib_alternative.dll' when provided as an argument" {
+        $output = . "$outPath/main.exe" "lib_alternative.dll"
+        
+        $output | Should -Be "1 + 2 = 4"
     }
 }
