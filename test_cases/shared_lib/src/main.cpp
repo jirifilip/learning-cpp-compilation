8f23b4cf96e4ed8c5a0686a5e8bac80b7de32efc
Jiří Filip <j.f.ilip@seznam.cz>
2024-12-06 15:06:58 +0100

    Add class for loading a library

--- test_cases/shared_lib/src/main.cpp
+++ test_cases/shared_lib/src/main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 
-typedef int (*plusFunc)(int, int);
+
 
 
 void abortWithMessage(const std::string& message) {
@@ -22,28 +22,46 @@ std::string resolveLibraryName(const std::vector<std::string>& arguments) {
 }
 
 
+class LoadedLibrary {
+private:
+    std::string name;
+    HINSTANCE library = nullptr;
+
+    typedef int (*plusFunc)(int, int);
+
+public:
+    plusFunc plus = nullptr;
+
+    LoadedLibrary(std::string name) : name(name) {
+        library = LoadLibrary(name.data());
+
+        if (library == NULL) {
+            abortWithMessage("Unable to load library");
+        }
+
+        plus = (plusFunc) GetProcAddress(library, "plus");
+
+        if (plus == NULL) {
+            abortWithMessage("Unable to get function address");
+        }
+    };
+
+    ~LoadedLibrary() {
+        if (library != nullptr) {
+            FreeLibrary(library);
+        }
+    }
+};
+
+
 int main(int argumentCount, char* argumentVector[]) {
     std::vector<std::string> arguments{ argumentVector + 1, argumentVector + argumentCount }; 
     std::string libraryName = resolveLibraryName(arguments);
     
-    HINSTANCE library = LoadLibrary(libraryName.data());
-
-    if (library == NULL) {
-        abortWithMessage("Unable to load library");
-    }
-
-    plusFunc plus = (plusFunc) GetProcAddress(library, "plus");
-
-    if (plus == NULL) {
-        abortWithMessage("Unable to get function address");
-        FreeLibrary(library);
-    }
-
-    int result = (plus)(1, 2);
+    LoadedLibrary lib { libraryName };
+    int result = lib.plus(1, 2);
 
     std::cout << "1 + 2 = " << result << std::endl;
-    
-    FreeLibrary(library);
 }
 
 
